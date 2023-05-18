9c6fa6cc54fbaddafcb36816a062e3e87b12eb19
DevxPi <macro.devxpi@gmail.com>
2023-05-18 16:25:21 +0800

    feat(app): create route and model for db

--- todo/app.py
+++ todo/app.py
@@ -1,15 +1,85 @@
-from flask import Flask
-from flask import render_template
+from flask import Flask, flash, render_template, request, redirect, url_for
+from flask_sqlalchemy import SQLAlchemy
+
+from os import environ, path
+from dotenv import load_dotenv
+
+BASEDIR = path.abspath(path.dirname(__file__))
+load_dotenv(path.join(BASEDIR, ".env"))
 
 app = Flask(__name__)
+app.config["SECRET_KEY"] = environ.get("SECRET_KEY")
 
-@app.route("/hello")
-def hello_world():
-    return "<p>Hello World</p>"
+
+# /// = relative path, //// = absolute path
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///db.sqlite"
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+db = SQLAlchemy(app)
+
+"""Model class"""
+
+
+class Todo(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100))
+    complete = db.Column(db.Boolean)
+
+
+"""Route """
+
 
 @app.route("/")
 def index():
-    return render_template('index.html', name='home')
+    todo_list = Todo.query.all()
+    return render_template("index.html", todo_list=todo_list)
+
+
+"""Create add method to add data"""
+
+
+@app.route("/add", methods=["POST"])
+def add():
+    title = request.form.get("title")
+    new_todo = Todo(title=title, complete=False)
+    db.session.add(new_todo)
+    db.session.commit()
+    flash("New data added...")
+
+    return redirect(url_for("index"))
+
+
+"""Update method for update existing data"""
+
+
+@app.route("/update/<int:todo_id>")
+def update(todo_id):
+    todo = Todo.query.filter_by(id=todo_id).first()
+    todo.complete = not todo.complete
+    db.session.commit()
+
+    flash(f"{todo.title} updated...")
+
+    return redirect(url_for("index"))
+
+
+"""Delete method for delete existing data"""
+
+
+@app.route("/delete/<int:todo_id>")
+def delete(todo_id):
+    todo = Todo.query.filter_by(id=todo_id).first()
+
+    if todo:
+        db.session.delete(todo)
+        db.session.commit()
+        flash(f"{todo.title} deleted...")
+    else:
+        flash("Data not available...")
+
+    return redirect(url_for("index"))
+
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    with app.app_context():
+        db.create_all()
+        app.run(debug=True)
